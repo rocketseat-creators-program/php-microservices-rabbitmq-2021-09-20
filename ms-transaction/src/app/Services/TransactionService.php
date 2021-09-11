a2d42c7439429fe10098cfc7b3dacbe8b74650c1
lucascavalcante <lucascavalcante@gmail.com>
2021-09-11 14:56:50 -0300

    Retrieving the payee information and building the payload to send to another microservice

--- ms-transaction/src/app/Services/TransactionService.php
+++ ms-transaction/src/app/Services/TransactionService.php
@@ -5,24 +5,35 @@ namespace App\Services;
 use App\Repositories\Interfaces\TransactionRepositoryInterface;
 use App\Services\Interfaces\TransactionServiceInterface;
 use App\Enums\Messages;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use App\Models\Transaction;
 
 class TransactionService implements TransactionServiceInterface
 {
-    public function __construct(private TransactionRepositoryInterface $repository)
-    {
+    public function __construct(
+        private TransactionRepositoryInterface $repository,
+        private UserRepositoryInterface $userRepository
+    ) {
     }
 
     public function process(array $data): string
     {
         $transaction = $this->repository->persist($data);
 
-        $this->sendNotification($transaction['payee']);
+        $this->sendNotification($transaction);
 
         return Messages::TRANSACTION_SUCCESSFULLY;
     }
 
-    private function sendNotification(int $payee)
+    private function sendNotification(Transaction $transaction): void
     {
-        return true;
+        $user = $this->userRepository->findById($transaction['payee']);
+
+        dd([
+            'transaction' => $transaction['id'],
+            'name' => $user['name'],
+            'email' => $user['email'],
+            'value' => $transaction['value']
+        ]);
     }
 }
