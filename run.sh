3c545eecfe1809b2b5d8926cb413015f02d336a9
lucascavalcante <lucascavalcante@gmail.com>
2021-09-11 16:29:39 -0300

    Improving project setup

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Starting the containers
 cd ms-transaction
 docker-compose up -d
 
@@ -8,3 +9,15 @@ docker-compose up -d
 
 cd ../ms-queues
 docker-compose up -d
+
+# ms-transaction container operations
+cd ../ms-transaction
+docker exec -it transaction-app cp .env.example .env
+docker exec -it transaction-app composer install
+docker exec -it transaction-app php artisan migrate
+docker exec -it transaction-app php artisan db:seed
+
+# ms-notification container operations
+cd ../ms-notification
+docker exec -it notification-app cp .env.example .env
+docker exec -it notification-app composer install
